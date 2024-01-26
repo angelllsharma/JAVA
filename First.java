a2b06b1445ae81aba65e9f9885894337b372655e
Angel Sharma <127425525+angelllsharma@users.noreply.github.com>
2024-01-26 20:45:20 +0530

    Add files via upload

---
+++ First.java
@@ -0,0 +1,9 @@
+//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+        //int a=2,b=7;
+        //System.out.println(a*=b);
+    }
+}
